0585d0a1348e4af28eb12203200d3487ba7862ce
diegoarize <diegoarize@outlook.com>
2015-06-13 19:56:22 -0300

    Criando mais TODO

--- src/EstrategiaDeValidacao.cpp
+++ src/EstrategiaDeValidacao.cpp
@@ -3,5 +3,9 @@
 //Implementação da classe
 void EstrategiaDeValidacao::validarPin(FormularioDoPin formPin)
 {
-	//TODO: Validar Pin somente se o numero de inserções for <=5 em um minuto.
+	//TODO: Validar Pin somente se o numero de inserções for <=5 em um minuto, verificando arquivo de log.
+
+	//TODO: Salvar em arquivo de log o timestamp da inserção de numero 1 até a ultima (fazer isso no após cadastro do pin).
+
+
 }
