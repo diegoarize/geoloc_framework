bd62f520358243f92bfecc2aca53b3f0ec095b08
diegoarize <diegoarize@outlook.com>
2015-06-13 19:51:33 -0300

    comentando TODO

--- src/SistemaMapa.h
+++ src/SistemaMapa.h
@@ -9,6 +9,8 @@ using namespace std;
 
 class Sistema_Mapa { 
 public :
+	//TODO: Criar um metodo main pra ser o inicializador da aplicação(talvez numa classe main)
+	//e isso instancia o Singleton e faz chamadas de input
 	static Sistema_Mapa instance; //Singleton
     list<Pin>  &listarPins();
 	Pin  buscarPin(const std::string &id) const;
